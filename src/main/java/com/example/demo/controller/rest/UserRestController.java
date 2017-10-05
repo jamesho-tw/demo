e81460ddb350a7aa642a06aee76bc6d867329c14
jamesho.tw <jamesho.tw@gmail.com>
2017-10-05 12:12:28 +0800

    Add logging.

--- src/main/java/com/example/demo/controller/rest/UserRestController.java
+++ src/main/java/com/example/demo/controller/rest/UserRestController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller.rest;
 
 import com.example.data.user.model.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +16,17 @@ import java.util.Collection;
 @RequestMapping("/user")
 public class UserRestController {
 
-    @RequestMapping(value = "/profile", method = RequestMethod.GET)
-    public ResponseEntity<?> readProfile() {
-        Collection<User> users = new ArrayList<User>();
-        users.add(new User("user1", "password", "salt", true));
-        users.add(new User("user2", "password", "salt", true));
-        return new ResponseEntity<Collection<User>>(users, HttpStatus.OK);
-    }
+  protected static final Logger logger = LoggerFactory.getLogger(UserRestController.class);
+
+  @RequestMapping(value = "/profile", method = RequestMethod.GET)
+  public ResponseEntity<?> readProfile() {
+    Collection<User> users = new ArrayList<User>();
+    users.add(new User("user1", "password", "salt", true));
+    users.add(new User("user2", "password", "salt", true));
+
+    logger.debug(String.format("USER SIZE: %d", users.size()));
+
+    return new ResponseEntity<Collection<User>>(users, HttpStatus.OK);
+  }
 
 }
